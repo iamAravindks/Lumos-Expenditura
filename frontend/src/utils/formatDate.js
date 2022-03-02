212322f95c1944090a7f19723c128b516c5bf820
iamAravindks <aravindkadaikattu@gmail.com>
2022-03-02 22:49:10 +0530

    🐛 Fix date issue

--- frontend/src/utils/formatDate.js
+++ frontend/src/utils/formatDate.js
@@ -1,7 +1,7 @@
 const formatDate = (date) => {
   const d = new Date(date);
   let month = `${d.getMonth() + 1}`;
-  let day = `${d.getDay()}`;
+  let day = `${d.getDate()}`;
   let year = `${d.getFullYear()}`;
 
   if (month.length < 2) {
