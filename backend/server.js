023ee160a7e76864903a8b85e35da1e1241c2c55
iamAravindks <aravindkadaikattu@gmail.com>
2022-10-01 22:58:08 +0530

    fix issue with db#1

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,9 @@ if (process.env.NODE_ENV === "production")
   app.use(express.static(path.join(__dirname, '../frontend/build')))
   app.get('*', (req, res) =>
   {
-    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+    const filePath = path.join(__dirname,'../frontend/build'
+    );
+    res.sendFile(path.resolve(filePath,'index.html'))
   })
 }
 
